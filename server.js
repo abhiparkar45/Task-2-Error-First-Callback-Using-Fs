aedc5d7acf23385a28773e49339c1a5ad08a0880
abhiparkar45 <abhishekparkar48@gmail.com>
2022-11-15 09:58:59 +0530

    v2.0.0

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const fs = require('fs');
+const fs = require("fs");
+
+const callbackFun = (err, data) => {
+  return err ? console.log(err.message) : console.log(data);
+};
 
 const renameFile = (oldName, newName, callback) => {
-    if(!oldName || !newName) {
-        callback({success:false,message:"File name cannot be null !"});
-        return;
-    }
-    if(!fs.existsSync(`./${oldName}`)) {
-        callback({success:false,message:"File does not exists"});
-        return;
-    }
-    var regex = /^[a-zA-Z0-9.]+$/;
-    if(!regex.test(newName)) {
-        callback({success:false,message:"File name cannot contains special characters"});
-        return;
-    }
-    fs.rename(oldName,newName, (error) => {
-        if (error) {
-            callback({success:false,message:"something went wrong !"})
-        }
-        callback({success:true,message:"File renamed successfully !"})
-    })
-}
+  if (!oldName || !newName) {
+    return callback(new Error("File name cannot be null !"));
+  }
+  if (!fs.existsSync(`./${oldName}`)) {
+    return callback(new Error("File does not exists"));
+  }
+  if (oldName === newName) {
+    return callback(new Error(`File name is already ${newName}`));
+  }
+  const regex = /^[a-zA-Z0-9.]+$/;
+  if (!regex.test(newName)) {
+    return callback(new Error("File name cannot contains special characters"));
+  }
+  fs.rename(oldName, newName, (error) => {
+    return error
+      ? callback(new Error("something went wrong"))
+      : callback(null, "File renamed successfully");
+  });
+};
 
-renameFile('abc','def',(res)=>{
-        console.log(res);
-});
+renameFile("abc.txt", "xyz.txt", callbackFun);
